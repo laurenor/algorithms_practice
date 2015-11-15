acfadb0c6cdd16c28434a015fa877661650babcb
Lauren <laurenortencio@gmail.com>
2015-11-15 03:07:54 -0800

    fixed errors.  Added power of 2 helper function

--- dec2bin/dec2bin.py
+++ dec2bin/dec2bin.py
@@ -1,5 +1,13 @@
+def is_power_of_2(num):
+    number = num
+    while number > 1:
+        if number % 2 == 1:
+            return False
+        else: 
+            number /= 2
+    return True
+
 def dec2bin(num):
-    from math import log
     """Convert a decimal number to binary representation.
 
     >>> dec2bin(0)
@@ -17,12 +25,15 @@ def dec2bin(num):
     >>> dec2bin(15)
     '1111'
     """
+    from math import log
     # multiples of 2
-    if log(num) % log(2) == 0:
-        return str(int(log(num)/log(2)))
+    if num == 0:
+        return str(0)
+    elif is_power_of_2(num):
+        return str(10**int(log(num)/log(2)))
     elif num % 2 != 0:
         # odd numbers
-        return dec2bin(num) + 1 
+        return str(int(dec2bin(num-1)) + 1)
     else:
         # numbers divisible by 2, but not powers of 2
         count = 0
@@ -31,7 +42,8 @@ def dec2bin(num):
             result /= 2
             count += 1
         remainder = num - 2**count
-        return 10**count + dec2bin(remainder)
+        return str(10**count + int(dec2bin(remainder)))
+
 
 if __name__ == '__main__':
     import doctest
