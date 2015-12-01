92cba2a962167fc1c57fd970cb9e1684e284d733
Lauren <laurenortencio@gmail.com>
2015-12-01 11:16:55 -0800

    added/completed rev-linked-list problem

---
+++ rev-linked-list/reversell.py
@@ -0,0 +1,50 @@
+class Node(object):
+    """Class in a linked list."""
+
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
+    def as_string(self):
+        """Represent data for this node and it's successors as a string.
+
+        >>> Node(3).as_string()
+        '3'
+
+        >>> Node(3, Node(2, Node(1))).as_string()
+        '321'
+        """
+
+        out = []
+        n = self
+
+        while n:
+            out.append(str(n.data))
+            n = n.next
+
+        return "".join(out)
+
+## my code below
+
+def reverse_linked_list(head):
+    """Given linked list head node, return head node of new, reversed linked list.
+
+    >>> ll = Node(1, Node(2, Node(3)))
+    >>> reverse_linked_list(ll).as_string()
+    '321'
+    """
+    reversed_head = None
+    current = None
+    original_head = head
+
+    while original_head:
+        current = original_head.data
+        reversed_head = Node(current, reversed_head)
+        original_head = original_head.next
+    return reversed_head
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. RIGHT ON!\n"
